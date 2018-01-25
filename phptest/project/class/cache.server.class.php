d63f9153953bf8b0f3fa10b042760115aa19d8b1
Carnagiul-MAC <pierrequeruel@gmail.com>
2018-01-25 03:16:35 +0100

    Adding cache server config

---
+++ phptest/project/class/cache.server.class.php
@@ -0,0 +1,8 @@
+<?php
+
+define ("ress_name", array("Fer", "Pierre", "Bois"))
+define ("server_speed_prod", array(1, 1, 1));
+define ("server_storage_increase", array(1, 1, 1));
+define ("server_increase_prod", array(1, 1, 1));
+
+?>
